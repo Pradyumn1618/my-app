8b8af2b1673691b6e466e2a97279ce01092a9cf9
Pradyumn1618 <pradyumnkangule@gmail.com>
2024-05-24 14:24:27 +0530

    done!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter as Router, Route, Navigate, Routes } from 'react-router-dom';
 import AdminPage from './admin';
 import LoginPage from './login';
@@ -19,40 +19,49 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [user,setUser]=useState(null);
-  
+  const [user, setUser] = useState(null);
+
 
   useEffect(() => {
-    setIsLoggedIn(JSON.parse(localStorage.getItem('isLoggedIn')) || false);
-    setIsAdmin(JSON.parse(localStorage.getItem('isAdmin')) || false);
-    setUser(JSON.parse(localStorage.getItem('user')) || null);
-    setIsLoading(false);
+    const updateLoginStatus = () => {
+      setIsLoggedIn(JSON.parse(localStorage.getItem('isLoggedIn')) || false);
+      setIsAdmin(JSON.parse(localStorage.getItem('isAdmin')) || false);
+      setUser(JSON.parse(localStorage.getItem('user')) || null);
+      setIsLoading(false);
+    };
+    updateLoginStatus();
+    window.addEventListener('storage', updateLoginStatus);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('storage', updateLoginStatus);
+    };
   }, []);
-  
+
   if (isLoading) {
-    return <div>Loading...</div>; // Replace this with your actual loading component
+    return <div>Loading...</div>;
   }
 
   return (
     <AuthContext.Provider value={{
       isLoggedIn, isAdmin, user, setIsLoggedIn, setIsAdmin, setUser
     }}>
-    <Router>
-      <div className="App">
-        <Routes>
-        <Route path="/" element={isLoggedIn ? <Navigate to="/user" replace/> : <Home/>} />
-        <Route path="/user" element={isLoggedIn && isAdmin ? <Navigate to="/admin" replace /> : <Navigate to="/" replace/>} />
-        <Route path="/login" element={isLoggedIn ? <Navigate to="/user" replace/>:<LoginPage />} />
-        <Route path="/admin" element={isLoggedIn && isAdmin ? <AdminPage /> : <Navigate to="/" replace />} />
-        <Route path="/signup" element={isLoggedIn ? <Navigate to="/user" replace/>:<SignupPage />} />
-        <Route path="/blog/:id" element={isLoggedIn ? <BlogPage />: <Navigate to="/" replace />} />
-        <Route path="/createBlog" element={isLoggedIn && isAdmin ? <Create/>  : <Navigate to="/" replace />}/>
-        <Route path="/editBlog" element={isLoggedIn && isAdmin ? <EditBlog/>  : <Navigate to="/" replace />}/>
-        <Route path="/edit/:id" element={isLoggedIn&&isAdmin ? <Edit/>:<Navigate to="/" replace/>}/>
-        <Route path="/AllUsers" element={isLoggedIn&&isAdmin? <AllUsers/>:<Navigate to="/" replace/>}/>
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={!isLoggedIn ? <Home /> : isAdmin ? <Navigate to="/admin" replace /> : <Navigate to="/user" replace />} />
+            <Route path="/user" element={isLoggedIn && !isAdmin ? <UserPage /> : <Navigate to="/" replace />} />
+            <Route path="/login" element={!isLoggedIn ? <LoginPage /> : isAdmin ? <Navigate to="/admin" replace /> : <Navigate to="/user" replace />} />
+            <Route path="/admin" element={isLoggedIn && isAdmin ? <AdminPage /> : <Navigate to="/" replace />} />
+            <Route path="/signup" element={!isLoggedIn ? <SignupPage /> : isAdmin ? <Navigate to="/admin" replace /> : <Navigate to="/user" replace />} />
+            <Route path="/blog/:id" element={isLoggedIn ? <BlogPage /> : <Navigate to="/login" replace />} />
+            <Route path="/createBlog" element={isLoggedIn && isAdmin ? <Create /> : <Navigate to="/login" replace />} />
+            <Route path="/editBlog" element={isLoggedIn && isAdmin ? <EditBlog /> : <Navigate to="/login" replace />} />
+            <Route path="/edit/:id" element={isLoggedIn && isAdmin ? <Edit /> : <Navigate to="/login" replace />} />
+            <Route path="/AllUsers" element={isLoggedIn && isAdmin ? <AllUsers /> : <Navigate to="/login" replace />} />
+          </Routes>
+        </div>
+      </Router>
     </AuthContext.Provider>
   );
 }
